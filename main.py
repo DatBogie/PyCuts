93eed0b4c0ab122e4fc2e0be00b8c1bc13bf171c
MCGEE MAXWELL P <MPMCGEE@williamsvillek12.org>
2025-04-28 10:52:10 -0400

    Update

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import os, sys, pynput
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        central = QWidget()
+        self.setCentralWidget(central)
+        
+        main_lay = QVBoxLayout()
+        central.setLayout(main_lay)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
